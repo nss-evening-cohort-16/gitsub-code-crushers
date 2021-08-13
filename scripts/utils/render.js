af49cedbe21e2a8b0ff1e23d19e92da2d9f1cc51
pmpurcell93 <maddenpurcell@gmail.com>
2021-08-12 21:08:24 -0500

    Added render page and builder functions

---
+++ scripts/utils/render.js
@@ -0,0 +1,43 @@
+
+
+
+
+//Madden
+const projectsCardBuilder = ()=> {
+    return;
+}
+
+
+const overviewFormBuilder= () => {
+    return;
+}
+
+
+repoFormBuilder = () => {
+    return;
+}
+
+// Austin
+
+const packageFormBuilder = () => {
+  return;
+};
+
+const projectsFormBuilder = () => {
+  return;
+};
+
+
+// Nathan 
+
+const overviewCardBuilder = () => {
+    return;
+};
+
+const repoCardBuilder = () => {
+    return;
+};
+
+const packageCardBuilder = () => {
+    retr
+};
